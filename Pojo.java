6041e0314e2dfeda7fd9519cb6915928198b03b3
metacube-mandeep-bhupal <mandeep.singh@st.niituniversity.in>
2016-08-25 16:53:03 +0530

    Create Pojo.java

---
+++ Pojo.java
@@ -0,0 +1,48 @@
+package assignment1;
+
+public class Pojo {
+
+	private String name;
+	private String publisher;
+	private String author;
+	private int acc_no;
+	private int title_id;
+	private int price;
+	
+	public void getName(String name)
+	{this.name=name;}
+	
+	public void setName(String name)
+	{this.name=name;}
+	
+	public void getPublisher(String publisher)
+	{this.publisher=publisher;}
+	
+	public void setPublisher(String publisher)
+	{this.publisher=publisher;}
+	
+	public void getAuthor(String author)
+	{this.author=author;}
+	
+	public void setAuthor(String author)
+	{this.author=author;}
+	
+	public void getAccNo(int acc_no)
+	{this.acc_no=acc_no;}
+	
+	public void setAccNo(int acc_no)
+	{this.acc_no=acc_no;}
+	
+	public void getTitleId(int title_id)
+	{this.title_id=title_id;}
+	
+	public void setTitleId(int title_id)
+	{this.title_id=title_id;}
+	
+	public void getPrice(int price)
+	{this.price=price;}
+	
+	public void setPrice(int price)
+	{this.price=price;}
+	
+}
