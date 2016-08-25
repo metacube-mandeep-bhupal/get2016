be70dea0768e7b4858bc04cb7c3d353bb73cdcf4
metacube-mandeep-bhupal <mandeep.singh@st.niituniversity.in>
2016-08-25 16:36:57 +0530

    Update question2.sql

--- DBMS-assignment--4/question2.sql
+++ DBMS-assignment--4/question2.sql
@@ -1,44 +1,39 @@
-/* Selecting the database */
 USE library;
 
-/* Create a View which can be used to display member name and
- * all issue details of the member using a simple SELECT command */
- CREATE VIEW member_issue_details AS
-    SELECT m.member_name, bi.issue_dt, bi.accession_number,
-        bi.member_id, bi.due_dt FROM book_issue bi
-        INNER JOIN members m ON bi.member_id = m.member_id;
-        
-/* Using the above view to display issue details of a member */
-SELECT member_name, issue_dt, due_dt, accession_number
-    FROM member_issue_details WHERE member_id = 'M16-004';
-    
-/* Create a View which contains three columns, member name, member id 
- * and full description of category, i.e., Faculty, Students 
- * and Others instead of F,S and O */
- CREATE VIEW member_category_description AS
-    SELECT member_name, member_id, 
-        CASE category
-            WHEN 'F' THEN 'Faculty'
-            WHEN 'S' THEN 'Student'
-            ELSE 'Others'
-        END as category FROM members;
-    
-/* Using the above view to display member category */
-SELECT member_id, member_name, category FROM member_category_description;
+/*Create a View which can be used to display member name and
+all issue details of the member using a simple SELECT command.*/
+Create View member_issue 
+AS
+select m.member_nm, bi.issue_dt,bi.accession_no,bi.member_id, bi.due_dt 
+from book_issue bi
+Left Join members m ON m.member_id=bi.member_id;
 
-/* Create a View which contains the information � subject name, title name, 
- * member name, category, issue date, due date and return date. If the 
- * books have not been returned, NULL should be displayed instead of return date */
- CREATE VIEW information_view AS
-    SELECT s.subject_name, t.title_name, m.member_name, m.category,
-        bi.issue_dt, bi.due_dt, br.return_dt FROM book_issue bi
-        INNER JOIN members m ON bi.member_id = m.member_id
-        INNER JOIN books b ON bi.accession_number = b.accession_number
-        INNER JOIN titles t ON b.title_id = t.title_id
-        INNER JOIN subjects s ON t.subject_id = s.subject_id
-        LEFT JOIN book_return br ON br.accession_number = bi.accession_number 
-            AND br.member_id = bi.member_id AND br.issue_dt = bi.issue_dt;
- 
- /* Using the above view to display the information */
-SELECT subject_name, title_name, member_name, category, issue_dt,
-    due_dt, return_dt FROM information_view;
+select member_nm,issue_dt,accession_no,member_id,due_dt from member_issue
+where member_id='101';
+
+/*Create a View which contains three columns, member name,
+member id and full description of category, i.e., Faculty, Students
+and Others instead of F,S and O*/
+Create View member_category
+AS
+select member_id,member_nm,
+CASE category
+   when 'F' then 'Faculty'
+   when 'S' then 'Student'
+   else 'Others'
+END AS category from members;   
+
+
+/*Create a View which contains the information – subject name,
+title, member name, category, issue date, due date and return
+date. If the books have not been returned, NULL should be
+displayed instead of return date.*/
+Create View information 
+AS
+select s.subject_nm,t.title_nm, m.member_nm,m.category,bi.issue_st,bi.due_dt,br.return_dt
+from book_issue bi
+Left Join members m ON m.member_id=bi.member_id
+Left Join book_return br ON br.accession_no=bi.accession_no AND br.member_id=bi.member_id AND br.issue_dt=bi.issue_dt
+Left Join books b ON b.accession_no=bi.accession_no
+Left Join titles t ON t.title_id=b.title_id
+Left Join subjects s ON s.subject_id=t.subject_id;
