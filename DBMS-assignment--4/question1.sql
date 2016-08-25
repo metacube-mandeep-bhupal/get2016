a544a9adec1e11b55c427f9ec8a3b81c86c2e4b3
metacube-mandeep-bhupal <mandeep.singh@st.niituniversity.in>
2016-08-25 16:35:54 +0530

    Update question1.sql

--- DBMS-assignment--4/question1.sql
+++ DBMS-assignment--4/question1.sql
@@ -1,41 +1,39 @@
-/* Selecting the database */
 USE library;
 
-/* To display name of those members who belong to the category as to 
- * which member 'Jon Snow' belongs. Use subquery */
- SELECT m.member_name AS 'Member Name' FROM members m WHERE m.category = 
-    (SELECT category FROM members c WHERE c.member_name = 'Jon Snow');
+/*Write a SELECT command to display name of those members
+who belong to the category as to which member
+“Jon Snow” belongs.*/
+select member_nm from members
+where category= (select category from members where member_nm = 'Jon Snow');
 
-/* To display information on the books that have not been returned till date. 
- * The information should include issue date, title name, member name 
- * and due date. Use Corelated query */
- SELECT bi.issue_dt AS 'Issue Date', t.title_name AS 'Title name',
-    m.member_name AS 'Member Name', bi.due_dt AS 'Due Date' FROM book_issue bi
-    INNER JOIN members m ON bi.member_id = m.member_id
-    INNER JOIN books b ON bi.accession_number = b.accession_number
-    INNER JOIN titles t ON b.title_id = t.title_id
-    WHERE (bi.issue_dt, bi.accession_number, bi.member_id) NOT IN
-    (SELECT br.issue_dt, br.accession_number, br.member_id FROM book_return br);
+/*Write a SELECT command to display information on the books
+that have not been returned till date. The information should
+include book issue date, title, member name and due date.*/
+select b.accession_no, bi.issue_dt,t.title_nm,m.member_nm,bi.due_dt
+from book_issue bi
+Inner Join members m ON m.member_id=bi.member_id
+Inner Join books b ON b.accession_no=bi.accession_no
+Inner Join titles t ON t.title_id=b.title_id
+where (bi.member_id,bi.accession_no,bi.issue_dt) NOT IN
+(select (br.member_id,br.accession_no,br.issue_dt) from book_return br);
 
-/* To display information on the books that have been returned after 
- * their due dates. The information should include issue date, title name,
- * member name and due date. Use Corelated query */
- SELECT bi.issue_dt AS 'Issue Date', t.title_name AS 'Title name',
-    m.member_name AS 'Member Name', bi.due_dt AS 'Due Date' FROM book_issue bi
-    INNER JOIN members m ON bi.member_id = m.member_id
-    INNER JOIN books b ON bi.accession_number = b.accession_number
-    INNER JOIN titles t ON b.title_id = t.title_id
-    WHERE (bi.issue_dt, bi.accession_number, bi.member_id) IN
-    (SELECT br.issue_dt, br.accession_number, br.member_id FROM book_return br
-        WHERE TIMESTAMPDIFF(DAY, bi.due_dt, br.return_dt) > 0);
+/*Write a SELECT command to display information on the books
+that have been returned after their due dates. The information
+should include book issue date, title, member name and due
+date.*/
+select b.accession_no, bi.issue_dt,t.title_nm,m.member_nm,bi.due_dt
+from book_issue bi
+Inner Join members m ON m.member_id=bi.member_id
+Inner Join books b ON b.accession_no=bi.accession_no
+Inner Join titles t ON t.title_id=b.title_id
+where bi.due_dt < (select br.return_dt from book_return br);
 
-/* To display information of those books whose price is equal to the 
- * booksmost expensive book purchase so far */
- SELECT b.accession_number AS 'Book Id', t.title_name AS 'Title Name'
-    FROM books b
-    INNER JOIN titles t ON b.title_id = t.title_id
-    WHERE b.price = (SELECT MAX(price) FROM books);
-    
-/* To display the second maximum price of a book */
-SELECT MAX(price) AS 'Second Max Price' FROM books
-    WHERE price != (SELECT MAX(price) FROM books);
+/*Write a SELECT command to display information of those books
+whose price is equal to the most expensive book purchase so far.*/
+select accession_no, title_id, purchase_dt,price, status from books 
+where price = (select MAX(price) from books);
+
+/*Write a SELECT command to display the second maximum price
+of a book.*/
+select MAX(price) from books 
+where price!= (select MAX(price) from books);
