abf1834906fd37b0247c8061c25949949541eb29
metacube-mandeep-bhupal <mandeep.singh@st.niituniversity.in>
2016-09-17 15:26:36 +0530

    Create assignment2.js

---
+++ jQuery-session2/assignment2/assignment2.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    
+    $("#hideFunction").click(function(){
+        $("span").hide();
+    });
+    
+    $("#showFunction").click(function(){
+        $("span").show();
+    });
+    
+    $("#toggleFunction").click(function(){
+        $("span").toggle();
+    });
+    
+});
